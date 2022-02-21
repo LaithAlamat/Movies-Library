898d9eed483a9a7931a2c0332254b03387284edd
LaithAlamat <laithalamat4@gmail.com>
2022-02-21 20:06:16 +0200

    Task12 submitted

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Movies-Library
+# Movies-Library - 1.0.0
 
 **Author Name**: Laith Alalamat
 
@@ -9,7 +9,53 @@
 
 ## Overview
 
-## Getting Started
+A simple project to create a movie library.
 
+## Getting Started
+Steps for building a server
+2/20/2022 Sunday
+1- Create server.js file
+
+2- npm init -y
+
+3- npm install express
+
+4- const express = require("express")
+
+5- const app = express()
+
+6- app.listen(PORT, ()=>{ console.log("Anything") })
+
+7- I can create end points (ex: app.get("/", helloWorldHandler))
+
+8- I will create the function for that end point(ex: helloWorldHandler)
+
+2/21/2022
+We continue building on the steps from the day before Additions
+
+1- Instead of reading from the json file we send a request to a third party API
+
+2- We read the documentation of the API and we tried on the Chrome or PostMan before we use it in our code.
+
+3- We found that we should create an API Key
+
+4- We installed Axios to send an HTTP request to the API.
+
+5- Axios is returning a promise so I should write my code that depend on the request result in the .then function.
+
+6- Axios is returng a huge object so I just care about the data property.
+
+7- We found the API key should not shown in my code so we created .env file and add the API Key there
+
+8- We installed dotenv library the configure it to make our server read the .env file.
+
+9- We created .env.example where we show the required variable for any developer will work on the same project.
+
+10- We created error handler: - We create a function. - We make our server know about it by using app.use - When i want to use I will use it in the .catch function.
+
+11- We created new end point that take the parameter from the URl where we found it inside req.query
 
 ## Project Features
+
+1- Get the data from third-party source.
+2- has some security with env file
--- server.js
+++ server.js
@@ -1,49 +1,134 @@
-'use strict'
-
+"use strict";
 
 const express = require("express");
-
+const moviesData = require("./Data/data.json");
 const app = express();
+const dotenv = require("dotenv");
+const axios = require("axios");
+dotenv.config();
+const APIKEY = process.env.APIKEY;
+const PORT = process.env.PORT;
 
-
-function MoviesLibrary(title, poster_path, overview){
-this.title = title;
-this.poster_path = poster_path;
-this.overview = overview;
+function MoviesLibrary(id, title, release_date, poster_path, overview) {
+  this.id = id;
+  this.title = title;
+  this.release_date = release_date;
+  this.poster_path = poster_path;
+  this.overview = overview;
 }
 
-app.listen(3000, ()=>{ 
-    console.log("listening to port 3000"); 
+app.listen(`${PORT}`, () => {
+  console.log(`Listen on ${PORT}`);
 });
 
-const moviesData = require("./Data/data.json");
+
 
 let homePageHandler = (req, res) => {
-    let moviesLibray = [];
-    moviesData.data.forEach((movie) => {
-      movie = new MoviesLibrary(movie.title, movie.poster_path, movie.overview);
-      moviesLibray.push(movie);
-    });
-    return res.status(200).json(moviesLibray);
-  };
+  let moviesLibray = [];
+  moviesData.data.forEach((movie) => {
+    movie = new MoviesLibrary(movie.id, movie.title, movie.release_date, movie.poster_path, movie.overview);
+    moviesLibray.push(movie);
+  });
+  return res.status(200).json(moviesLibray);
+};
 
 app.get("/", homePageHandler);
 
 let favoritePageHandler = (req, res) => {
-    return res.status(200).send("Welcome to Favorite Page");
-  };
-  
+  return res.status(200).send("Welcome to Favorite Page");
+};
+
 app.get("/favorite", favoritePageHandler);
 
-let pageNotFoundHandler = (req, res) => {
-    res.send({
-        status: 404,
-        responseText: "Page not found.",
+let trendingPageHandler = (req, res) => {
+  let trendingMovies = [];
+  axios.get(`https://api.themoviedb.org/3/trending/all/week?api_key=${APIKEY}&language=en-US`)
+    .then((value) => {
+      value.data.results.forEach((movie) => {
+        movie = new MoviesLibrary(
+          movie.id,
+          movie.title,
+          movie.release_date,
+          movie.poster_path,
+          movie.overview
+        );
+        trendingMovies.push(movie);
       });
-    };
-  
-  app.get("*", pageNotFoundHandler);
+      return res.status(200).json(trendingMovies);
+    });
+};
+
+app.get("/trending", trendingPageHandler);
+
+let topRatedPageHandler = (req, res) => {
+  let topRated = [];
+  axios.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=${APIKEY}&language=en-US&page=1`)
+    .then((value) => {
+      value.data.results.forEach((movie) => {
+        movie = new MoviesLibrary(
+          movie.id,
+          movie.title,
+          movie.release_date,
+          movie.poster_path,
+          movie.overview
+        );
+        topRated.push(movie);
+      });
+      return res.status(200).json(topRated);
+    });
+};
+
+app.get("/topRated", topRatedPageHandler);
+
+let upcomingPageHandler = (req, res) => {
+  let upcoming = [];
+  axios.get(`https://api.themoviedb.org/3/movie/upcoming?api_key=${APIKEY}&language=en-US&page=1`)
+    .then((value) => {
+      value.data.results.forEach((movie) => {
+        movie = new MoviesLibrary(
+          movie.id,
+          movie.title,
+          movie.release_date,
+          movie.poster_path,
+          movie.overview
+        );
+        upcoming.push(movie);
+      });
+      return res.status(200).json(upcoming);
+    });
+};
+
+app.get("/upcoming", upcomingPageHandler);
+
+let searchPageHandler = (req, res) => {
+  let searchQuery = req.query.search;
+  console.log(req.query.search);
+  let searchResults = [];
+
+  axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${APIKEY}&language=en-US&query=${searchQuery}`)
+    .then((value) => {
+      value.data.results.forEach((movie) => {
+        movie = new MoviesLibrary(
+          movie.id,
+          movie.title,
+          movie.release_date,
+          movie.poster_path,
+          movie.overview
+        );
+        searchResults.push(movie);
+      });
+      return res.status(200).json(searchResults);
+    });
+};
+
+app.get("/search", searchPageHandler);
 
 
+let pageNotFoundHandler = (req, res) => {
+  res.send({
+    status: 404,
+    responseText: "Page not found.",
+  });
+};
 
-
+app.get("*", pageNotFoundHandler);
